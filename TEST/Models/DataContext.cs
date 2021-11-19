8b2a08457ca28f4ae41c5840a7f894afba7062f2
ZolarComics <srgjzln@gmail.com>
2021-11-19 18:56:25 +1000

    Add DB

---
+++ TEST/Models/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TEST.Models
+{
+    public class DataContext : DbContext
+    { 
+            public DbSet<Shmot> Shmots { get; set; }
+            public DbSet<Сheck> Checks { get; set; }
+
+            public DataContext(DbContextOptions<DataContext> options)
+                : base(options)
+            {
+                Database.EnsureCreated();
+            }
+        
+    }
+}
---
+++ TEST/Models/Shmot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TEST.Models
+{
+    public class Shmot
+    {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Size { get; set; }
+            public int Price { get; set; }
+    }
+}
